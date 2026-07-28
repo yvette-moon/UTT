b720584f6b41b12e33d046eae0dc21063865af0c
yvette <19006565288@163.com>
2026-07-28 14:22:37 +0800

    初始设想架构

--- dataprovider/data_factory.py
+++ dataprovider/data_factory.py
@@ -1,6 +1,6 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
+from dataprovider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
     MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
-from data_provider.uea import collate_fn
+from dataprovider.uea import collate_fn
 from torch.utils.data import DataLoader
 
 data_dict = {
--- dataprovider/data_loader.py
+++ dataprovider/data_loader.py
@@ -7,13 +7,11 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 from utils.timefeatures import time_features
-from data_provider.m4 import M4Dataset, M4Meta
-from data_provider.uea import subsample, interpolate_missing, Normalizer
-from sktime.datasets import load_from_tsfile_to_dataframe
+from dataprovider.m4 import M4Dataset, M4Meta
+from dataprovider.uea import subsample, interpolate_missing, Normalizer
+
 import warnings
-from utils.augmentation import run_augmentation_single
-from datasets import load_dataset
-from huggingface_hub import hf_hub_download
+
 warnings.filterwarnings('ignore')
 
 HUGGINGFACE_REPO = "thuml/Time-Series-Library"
